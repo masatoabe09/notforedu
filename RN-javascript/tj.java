4c480a21ba4bf94f05b10c878114078e2d4d78db
suzukiweb23 <suzukiweb23@gmail.com>
2018-05-09 13:05:54 +0900

    Create tj.java

---
+++ RN-javascript/tj.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        System.out.println("ok! Hello World!!!");
+        
+    }
+}
