6f125eb69fa0b79aa12138a882e156917e664434
First-name Family-name <username@example.com>
2018-05-21 14:33:58 +0900

    cpp

--- RN-javascript/test.cpp
+++ RN-javascript/test.cpp
@@ -1,5 +1,11 @@
  #include <stdio.h>
+ #include <iostream>
+
   int main() {
       printf("Hello World");
-             return 0;
+      std::cout << "Hello iostream" << std::endl;
+      std::cout << "2 + 2 = " << 2 + 2 << std::endl;
+      return 0;
        }
+
+ 
