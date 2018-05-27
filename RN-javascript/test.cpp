adc00a0a5c5def7ed4432b2fa28962ef4e385a88
RyosukeNishimura <horizontal.sound@gmail.com>
2018-05-27 21:36:45 +0900

    message

--- RN-javascript/test.cpp
+++ RN-javascript/test.cpp
@@ -1,5 +1,11 @@
  #include <stdio.h>
+ #include <iostream>
+
   int main() {
       printf("Hello World");
-             return 0;
+      std::cout << "Hello iostream" << std::endl;
+      std::cout << "2 + 2 = " << 2 + 2 << std::endl;
+      return 0;
        }
+//http://www.ced.is.utsunomiya-u.ac.jp/lecture/2014/prog/p3/kadai2/page1.php
+ 
