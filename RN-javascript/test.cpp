2b8a73de22f39d45d68774596404366ad6d08dbd
First-name Family-name <username@example.com>
2018-05-28 12:52:20 +0900

    test

--- RN-javascript/test.cpp
+++ RN-javascript/test.cpp
@@ -1,12 +1,12 @@
  #include <stdio.h>
  #include <iostream>
 
-  int main() {
-      printf("Hello World");
-      std::cout << "Hello iostream" << std::endl;
-      std::cout << "2 + 2 = " << 2 + 2 << std::endl;
-      return 0;
-       }
+//  int main() {
+//      printf("Hello World");
+//      std::cout << "Hello iostream" << std::endl;
+//      std::cout << "2 + 2 = " << 2 + 2 << std::endl;
+//      return 0;
+//       }
 //http://www.ced.is.utsunomiya-u.ac.jp/lecture/2014/prog/p3/kadai2/page1.php
  
 int main() {
