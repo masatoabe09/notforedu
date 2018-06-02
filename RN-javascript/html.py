b052ced0aa5006e5aa88085c5092fe10a5021e54
RyosukeNishimura <horizontal.sound@gmail.com>
2018-06-02 20:28:20 +0900

    message

--- RN-javascript/html.py
+++ RN-javascript/html.py
@@ -57,13 +57,42 @@ x = 5
 y = 3
 a = x - y
 b = x * y
-C = a + b
+c = a + b
+c
 
 z = "Hello World"
 z
 
 #出力して結果を確かめよう。
 
-
+z="Hello"
+a="World"
 print(z,a)
 
+2 != 3
+
+z = "Hello World"
+z == "Hello World"
+
+z == "Hello WOrld"
+
+1 == 1 and 2 < 10 and 3 != 10
+
+1 == 2 or 2 > 3 or 5 != 8
+
+not 1 == 1
+
+Country = "Japan"
+if Country == "Japan":
+ print("You're in Japan")
+else:
+ print("You're not in Japan")
+
+x = "Hello"
+y = "World"
+
+if x == "Hell":
+ if y == "World":
+  print(x + y)
+else:
+  print("Wrong Word")
